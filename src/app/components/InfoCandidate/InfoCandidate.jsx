29332e24d49b13d9fb2b5b83cf588d90b893caa6
Pvvirg <pvvirg16@gmail.com>
2024-03-23 22:53:56 -0300

    Comentários criados no componente InfoCandidate

--- src/app/components/InfoCandidate/InfoCandidate.jsx
+++ src/app/components/InfoCandidate/InfoCandidate.jsx
@@ -3,11 +3,15 @@ import styles from "./infoCandidate.module.css";
 export default function InfoCandidate() {
   return (
     <section className={styles.container}>
+      {/* Container for candidate information */}
       <div className={styles.contentInfoCandidate}>
+        {/* Heading */}
         <h1 className={styles.nameInfoCandidate}>
           Quem é <span className={styles.textBackground}>Ciro Nogueira?</span>
         </h1>
+        {/* Content */}
         <div className={styles.contentTextCandidate}>
+          {/* Paragraph */}
           <p className={styles.paragraphCandidate}>
             Ciro Nogueira é Senador pelo estado do Piauí. Nascido em 21 de
             novembro de 1968 em Teresina, Piauí, Ciro Nogueira é formado em
@@ -18,18 +22,22 @@ export default function InfoCandidate() {
             Desde 2013, Ciro Nogueira preside o Progressistas, um dos maiores
             partidos políticos do Brasil
           </p>
+          {/* Quote */}
           <span className={styles.phraseCandidate}>
             <b className={styles.marksColor}>“</b>Decidi entrar na politica
             depois de ver tanta desigualdade onde eu vivia.
             <b className={styles.marksColor}>”</b>
           </span>
+          {/* Subheading */}
           <h4 className={styles.birthCandidate}>
             nascido em 1997 <br />
             <b className={styles.nameTextBold}>ciro nogueira</b>
           </h4>
         </div>
       </div>
+      {/* Container for candidate image */}
       <div className={styles.contentImgCandidate}>
+        {/* Candidate image */}
         <img
           className={styles.ImgCandidate}
           src="/images/cironogueira.png"
