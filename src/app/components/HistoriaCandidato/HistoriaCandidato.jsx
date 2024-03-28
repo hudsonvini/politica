5f59b31d18e6176cbbcb54ed6b4878143500e4ef
Hudson <hudsonvini26@gmail.com>
2024-03-28 00:48:11 -0300

    Normalizando os estilos das páginas blog e noticias

--- src/app/components/HistoriaCandidato/HistoriaCandidato.jsx
+++ src/app/components/HistoriaCandidato/HistoriaCandidato.jsx
@@ -1,3 +1,4 @@
+import { fontSecundaria } from '@/app/fonts'
 import style from './HistoriaCandidato.module.css'
 
 export default function HistoriaCandidato() {
@@ -12,7 +13,7 @@ export default function HistoriaCandidato() {
                 {/* Info Area */}
                 <div className={style.infoArea}>
                     <h1>Quem é <span>Ciro Nogueira?</span></h1>
-                    <p>
+                    <p className={fontSecundaria.className}>
                         Ciro Nogueira é Senador pelo estado do Piauí. Nascido em 21 de novembro de 1968 em Teresina, Piauí, Ciro Nogueira é formado em Direito na PUC do Rio de Janeiro. Foi Ministro de Estado Chefe da Casa Civil da Presidência da República durante o governo do presidente Bolsonaro (2021-2022). Foi deputado federal por quatro mandatos e chegou ao Senado em 2011, sendo eleito novamente em 2018. Desde 2013, Ciro Nogueira preside o Progressistas, um dos maiores partidos políticos do Brasil
                         <br /><br />
                         Ciro Nogueira é Senador pelo estado do Piauí. Nascido em 21 de novembro de 1968 em Teresina, Piauí, Ciro Nogueira é formado em Direito na PUC do Rio de Janeiro. Foi Ministro de Estado Chefe da Casa Civil da Presidência da República durante o governo do presidente Bolsonaro (2021-2022). Foi deputado federal por quatro mandatos e chegou ao Senado em 2011, sendo eleito novamente em 2018. Desde 2013, Ciro Nogueira preside o Progressistas, um dos maiores partidos políticos do Brasil
