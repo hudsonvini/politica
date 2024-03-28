f8490147b10e9113207e363fc95d185397b7dddb
Hudson <hudsonvini26@gmail.com>
2024-03-28 17:24:58 -0300

    Arrumando Problemas de Responsividade

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -51,7 +51,7 @@ export function Header() {
             height="34"
             viewBox="0 0 24 24"
             stroke-width="1.5"
-            stroke="#2c3e50"
+            stroke="#1185CE"
             fill="none"
             stroke-linecap="round"
             stroke-linejoin="round"
