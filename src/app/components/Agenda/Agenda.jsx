0a22d82bef7773851c15e73b89f0fd844ee398e7
Hudson <hudsonvini26@gmail.com>
2024-04-12 09:00:35 -0300

    Add new style on Agenda and Comment Initial Banner

--- src/app/components/Agenda/Agenda.jsx
+++ src/app/components/Agenda/Agenda.jsx
@@ -28,7 +28,7 @@ export default function Agenda() {
                             {/* Spot */}
                             <span className={`${style.spot} ${fontSecundaria.className}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
-                                <p>Radio Bandeirantes</p>
+                                <p>Teresina, Piauí</p>
                             </span>
                         </div>
                     </div>
@@ -43,18 +43,18 @@ export default function Agenda() {
 
                     {/* Information area */}
                     <div className={style.informationArea}>
-                        <h3>Entrevista na radio bandeirantes</h3>
+                        <h3>Passeata pela liberdade</h3>
                         
                         <div className={style.inferiorInformation}>
                             {/* Spot */}
                             <span className={`${style.spot} ${fontSecundaria.className}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
-                                <p>14:00</p>
+                                <p>15:00</p>
                             </span>
                             {/* Spot */}
                             <span className={`${style.spot} ${fontSecundaria.className}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
-                                <p>Radio Bandeirantes</p>
+                                <p>Campinas, São Paulo</p>
                             </span>
                         </div>
                     </div>
@@ -69,18 +69,18 @@ export default function Agenda() {
 
                     {/* Information area */}
                     <div className={style.informationArea}>
-                        <h3>Entrevista na radio bandeirantes</h3>
+                        <h3>Sabatina - TV Cidade Verde</h3>
                         
                         <div className={style.inferiorInformation}>
                             {/* Spot */}
                             <span className={`${style.spot} ${fontSecundaria.className}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
-                                <p>14:00</p>
+                                <p>12:00</p>
                             </span>
                             {/* Spot */}
                             <span className={`${style.spot} ${fontSecundaria.className}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
-                                <p>Radio Bandeirantes</p>
+                                <p>Teresina, Piauí</p>
                             </span>
                         </div>
                     </div>
@@ -95,7 +95,7 @@ export default function Agenda() {
 
                     {/* Information area */}
                     <div className={style.informationArea}>
-                        <h3>Entrevista na radio bandeirantes</h3>
+                        <h3>Entrevista na Radio Bandeirantes</h3>
                         
                         <div className={style.inferiorInformation}>
                             {/* Spot */}
@@ -106,7 +106,7 @@ export default function Agenda() {
                             {/* Spot */}
                             <span className={`${style.spot} ${fontSecundaria.className}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
-                                <p>Radio Bandeirantes</p>
+                                <p>São Caetano - São Paulo</p>
                             </span>
                         </div>
                     </div>
@@ -121,18 +121,18 @@ export default function Agenda() {
 
                     {/* Information area */}
                     <div className={style.informationArea}>
-                        <h3>Entrevista na radio bandeirantes</h3>
+                        <h3>Entrevista Rede Globo de Televisão</h3>
                         
                         <div className={style.inferiorInformation}>
                             {/* Spot */}
                             <span className={`${style.spot} ${fontSecundaria.className}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
-                                <p>14:00</p>
+                                <p>19:00</p>
                             </span>
                             {/* Spot */}
                             <span className={`${style.spot} ${fontSecundaria.className}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
-                                <p>Radio Bandeirantes</p>
+                                <p>Rio de Janeiro - RJ</p>
                             </span>
                         </div>
                     </div>
@@ -147,18 +147,44 @@ export default function Agenda() {
 
                     {/* Information area */}
                     <div className={style.informationArea}>
-                        <h3>Entrevista na radio bandeirantes</h3>
+                        <h3>Passeata de apoio ao Ciro</h3>
                         
                         <div className={style.inferiorInformation}>
                             {/* Spot */}
                             <span className={`${style.spot} ${fontSecundaria.className}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
-                                <p>14:00</p>
+                                <p>13:00</p>
                             </span>
                             {/* Spot */}
                             <span className={`${style.spot} ${fontSecundaria.className}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
-                                <p>Radio Bandeirantes</p>
+                                <p>Parnaíba - Piauí</p>
+                            </span>
+                        </div>
+                    </div>
+                </article>
+
+                {/* Card */}
+                <article className={style.card}>
+                    {/* Data Area */}
+                    <div className={style.dataArea}>
+                        14
+                    </div>
+
+                    {/* Information area */}
+                    <div className={style.informationArea}>
+                        <h3>Corrida pela liberdade</h3>
+                        
+                        <div className={style.inferiorInformation}>
+                            {/* Spot */}
+                            <span className={`${style.spot} ${fontSecundaria.className}`}>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
+                                <p>07:00</p>
+                            </span>
+                            {/* Spot */}
+                            <span className={`${style.spot} ${fontSecundaria.className}`}>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+                                <p>Teresina - Piauí</p>
                             </span>
                         </div>
                     </div>
@@ -184,33 +210,7 @@ export default function Agenda() {
                             {/* Spot */}
                             <span className={`${style.spot} ${fontSecundaria.className}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
-                                <p>Radio Bandeirantes</p>
-                            </span>
-                        </div>
-                    </div>
-                </article>
-
-                {/* Card */}
-                <article className={style.card}>
-                    {/* Data Area */}
-                    <div className={style.dataArea}>
-                        14
-                    </div>
-
-                    {/* Information area */}
-                    <div className={style.informationArea}>
-                        <h3>Entrevista na radio bandeirantes</h3>
-                        
-                        <div className={style.inferiorInformation}>
-                            {/* Spot */}
-                            <span className={`${style.spot} ${fontSecundaria.className}`}>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
-                                <p>14:00</p>
-                            </span>
-                            {/* Spot */}
-                            <span className={`${style.spot} ${fontSecundaria.className}`}>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
-                                <p>Radio Bandeirantes</p>
+                                <p>Espirito Santo - Belo horizonte</p>
                             </span>
                         </div>
                     </div>
