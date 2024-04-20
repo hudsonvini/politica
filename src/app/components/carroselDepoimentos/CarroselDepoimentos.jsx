829d2568a3c046099454ef2e126540eb2135efa8
Hudson <hudsonvini26@gmail.com>
2024-04-20 18:10:09 -0300

    Add new style and responsividade to carrosel Depoimentos component

--- src/app/components/carroselDepoimentos/CarroselDepoimentos.jsx
+++ src/app/components/carroselDepoimentos/CarroselDepoimentos.jsx
@@ -62,7 +62,7 @@ export default function CarroselDepoimentos() {
                     
                     {/* Card */}
                     <article className={style.card}>
-                        <img src="https://static.ndmais.com.br/2023/11/pedreiro-brincalhao-600x800.png" alt="" />
+                        <img src="https://www.camara.leg.br/midias/image/2023/10/img20231026113029739-768x473.jpg" alt="" />
 
                         <div className={style.areaInformation}>
                             <h1>Dona Maria do Socorro</h1>
@@ -123,19 +123,7 @@ export default function CarroselDepoimentos() {
                     </article>
                     {/* Card */}
                     <article className={style.card}>
-                        <img src="https://static.ndmais.com.br/2023/11/pedreiro-brincalhao-600x800.png" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Dona Maria do Socorro</h1>
-                            <div className={style.areaSpans}>
-                                <span>Designer</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
-                    </article>
-                    {/* Card */}
-                    <article className={style.card}>
-                        <img src="https://static.ndmais.com.br/2023/11/pedreiro-brincalhao-600x800.png" alt="" />
+                        <img src="https://www.camara.leg.br/midias/image/2023/10/img20231026113029739-768x473.jpg" alt="" />
 
                         <div className={style.areaInformation}>
                             <h1>Dona Maria do Socorro</h1>
@@ -155,7 +143,7 @@ export default function CarroselDepoimentos() {
                 <div className={style.SlideContainer}>
                     {/* Card */}
                     <article className={style.card}>
-                        <img src="https://f.i.uol.com.br/fotografia/2023/08/25/169299396464e909ac19018_1692993964_3x2_md.jpg" alt="" />
+                        <img src="https://neon.com.br/aprenda/wp-content/uploads/2023/08/quanto-ganha-engenheiro-civil.png" alt="" />
 
                         <div className={style.areaInformation}>
                             <h1>Jessíca Donato</h1>
@@ -167,7 +155,7 @@ export default function CarroselDepoimentos() {
                     </article>
                     {/* Card */}
                     <article className={style.card}>
-                        <img src="https://www.cartacapital.com.br/wp-content/uploads/2019/11/cancerdemama-mulheres.jpg" alt="" />
+                        <img src="https://www.acif.org.br/wp-content/uploads/2023/10/jovem-empreendedor-scaled-1.jpg" alt="" />
 
                         <div className={style.areaInformation}>
                             <h1>Dora Medeiros</h1>
@@ -179,7 +167,7 @@ export default function CarroselDepoimentos() {
                     </article>
                     {/* Card */}
                     <article className={style.card}>
-                        <img src="https://assets-blog.pagseguro.uol.com.br/wp-content/2022/08/Auxilio-Taxista.jpg" alt="" />
+                        <img src="https://mediflix.com.br/wp-content/uploads/2020/03/7781-scaled-1.jpg" alt="" />
 
                         <div className={style.areaInformation}>
                             <h1>Jorge Medeiros</h1>
@@ -191,7 +179,20 @@ export default function CarroselDepoimentos() {
                     </article>
                     {/* Card */}
                     <article className={style.card}>
-                        <img src="https://static.ndmais.com.br/2023/11/pedreiro-brincalhao-600x800.png" alt="" />
+                        <img src="https://goliveira.adv.br/wp-content/uploads/2022/04/Aposentadoria-especial-mecanico.jpg" alt="" />
+
+                        <div className={style.areaInformation}>
+                            <h1>Dona Maria do Socorro</h1>
+                            <div className={style.areaSpans}>
+                                <span>Designer</span>
+                                <span>47 anos</span>
+                            </div>
+                        </div>
+                    </article>
+                    
+                    {/* Card */}
+                    <article className={style.card}>
+                        <img src="https://salario2024.com.br/wp-content/uploads/2023/05/domestic-routine-smiling-housewife-apron-standing-kitchen-scaled-e1685535865342.jpg" alt="" />
 
                         <div className={style.areaInformation}>
                             <h1>Dona Maria do Socorro</h1>
@@ -204,20 +205,7 @@ export default function CarroselDepoimentos() {
 
                     {/* Card */}
                     <article className={style.card}>
-                        <img src="https://f.i.uol.com.br/fotografia/2023/08/25/169299396464e909ac19018_1692993964_3x2_md.jpg" alt="" />
-
-                        <div className={style.areaInformation}>
-                            <h1>Jessíca Donato</h1>
-                            <div className={style.areaSpans}>
-                                <span>Medica</span>
-                                <span>47 anos</span>
-                            </div>
-                        </div>
-                    </article>
-
-                    {/* Card */}
-                    <article className={style.card}>
-                        <img src="https://f.i.uol.com.br/fotografia/2023/08/25/169299396464e909ac19018_1692993964_3x2_md.jpg" alt="" />
+                        <img src="https://neon.com.br/aprenda/wp-content/uploads/2023/08/quanto-ganha-engenheiro-civil.png" alt="" />
 
                         <div className={style.areaInformation}>
                             <h1>Jessíca Donato</h1>
@@ -229,7 +217,7 @@ export default function CarroselDepoimentos() {
                     </article>
                     {/* Card */}
                     <article className={style.card}>
-                        <img src="https://www.cartacapital.com.br/wp-content/uploads/2019/11/cancerdemama-mulheres.jpg" alt="" />
+                        <img src="https://www.acif.org.br/wp-content/uploads/2023/10/jovem-empreendedor-scaled-1.jpg" alt="" />
 
                         <div className={style.areaInformation}>
                             <h1>Dora Medeiros</h1>
@@ -241,7 +229,7 @@ export default function CarroselDepoimentos() {
                     </article>
                     {/* Card */}
                     <article className={style.card}>
-                        <img src="https://assets-blog.pagseguro.uol.com.br/wp-content/2022/08/Auxilio-Taxista.jpg" alt="" />
+                        <img src="https://mediflix.com.br/wp-content/uploads/2020/03/7781-scaled-1.jpg" alt="" />
 
                         <div className={style.areaInformation}>
                             <h1>Jorge Medeiros</h1>
@@ -253,7 +241,7 @@ export default function CarroselDepoimentos() {
                     </article>
                     {/* Card */}
                     <article className={style.card}>
-                        <img src="https://static.ndmais.com.br/2023/11/pedreiro-brincalhao-600x800.png" alt="" />
+                        <img src="https://goliveira.adv.br/wp-content/uploads/2022/04/Aposentadoria-especial-mecanico.jpg" alt="" />
 
                         <div className={style.areaInformation}>
                             <h1>Dona Maria do Socorro</h1>
@@ -265,7 +253,7 @@ export default function CarroselDepoimentos() {
                     </article>
                     {/* Card */}
                     <article className={style.card}>
-                        <img src="https://static.ndmais.com.br/2023/11/pedreiro-brincalhao-600x800.png" alt="" />
+                        <img src="https://salario2024.com.br/wp-content/uploads/2023/05/domestic-routine-smiling-housewife-apron-standing-kitchen-scaled-e1685535865342.jpg" alt="" />
 
                         <div className={style.areaInformation}>
                             <h1>Dona Maria do Socorro</h1>
