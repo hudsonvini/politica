d94769ffb6b12f2e245c28846b0a0dfe5b2942af
Hudson <hudsonvini26@gmail.com>
2024-04-21 20:03:18 -0300

    Add hover Effect on View more info buttons and new stle for newslletter

--- src/app/components/Agenda/Agenda.jsx
+++ src/app/components/Agenda/Agenda.jsx
@@ -40,8 +40,15 @@ function AgendaCard(props) {
 export default function Agenda() {
   return (
     <section className={style.container}>
-      {/* Title */}
-      <h2 className={style.title}>Agenda</h2>
+      {/* ContainerTitle */}
+      <div className={style.containerTitle}>
+        <h2 className={style.titleBlog}>Agenda</h2> {/* Title of the blog */}
+        <a className={style.viewMore} href="/agenda">
+          Ver
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
+        </a>
+      </div>
+
       {/* Area Agenda */}
       <div className={style.areaAgenda}>
         <AgendaCard
